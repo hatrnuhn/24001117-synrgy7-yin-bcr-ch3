9e9f4d7cbfe7f0bdaedebf5e8927d7320d596118
hatrnuhn <yusron.ibnunaim@gmail.com>
2024-04-12 07:40:35 +0700

    add from scratch sort functions

--- scripts/sortCarByYearAscendingly.js
+++ scripts/sortCarByYearAscendingly.js
@@ -5,7 +5,44 @@ function sortCarByYearAscendingly(cars) {
   // Clone array untuk menghindari side-effect
   const result = [...cars];
 
+  // update setelah submit form
+  // merge sort source: en.wikipedia
+  function sortAsc(array) {
+    const arrayWork = [...array], arrayCopy = [...array];
+    splitMerge(arrayWork, 0, arrayCopy.length, arrayCopy);
+    return arrayWork;
+  }
+  
+  function splitMerge(B, iBegin, iEnd, A) {
+    if (iEnd - iBegin <= 1) {
+      return;
+    }
+  
+    const iMid = Math.floor((iEnd + iBegin) / 2);
+  
+    splitMerge(A, iBegin, iMid, B);
+    splitMerge(A, iMid, iEnd, B);
+  
+    merge(B, iBegin, iMid, iEnd, A);
+  }
+  
+  function merge(B, iBegin, iMid, iEnd, A) {
+    let i = iBegin, j = iMid;
+    for (let k = iBegin; k < iEnd; k++) {
+      if (i < iMid && (j >= iEnd || A[i]['year'] <= A[j]['year'])) {
+        B[k] = A[i];
+        i = i + 1;
+      } else {
+        B[k] = A[j];
+        j = j + 1;
+      }
+    }
+  }
+
+  return sortAsc(result);
+  /*
   return result.sort((a, b) => {
     return a['year'] - b['year'];
   });
+  */
 }
--- scripts/sortCarByYearDecendingly.js
+++ scripts/sortCarByYearDecendingly.js
@@ -5,7 +5,45 @@ function sortCarByYearDescendingly(cars) {
   // Clone array untuk menghindari side-effect
   const result = [...cars];
 
+  // update setelah submit form
+  // merge sort source: en.wikipedia
+  function sortDesc(array) {
+    const arrayWork = [...array], arrayCopy = [...array];
+    splitMerge(arrayWork, 0, arrayCopy.length, arrayCopy);
+    return arrayWork;
+  }
+  
+  function splitMerge(B, iBegin, iEnd, A) {
+    if (iEnd - iBegin <= 1) {
+      return;
+    }
+  
+    const iMid = Math.floor((iEnd + iBegin) / 2);
+  
+    splitMerge(A, iBegin, iMid, B);
+    splitMerge(A, iMid, iEnd, B);
+  
+    merge(B, iBegin, iMid, iEnd, A);
+  }
+  
+  function merge(B, iBegin, iMid, iEnd, A) {
+    let i = iBegin, j = iMid;
+    for (let k = iBegin; k < iEnd; k++) {
+      if (i < iMid && (j >= iEnd || A[i]['year'] >= A[j]['year'])) {
+        B[k] = A[i];
+        i = i + 1;
+      } else {
+        B[k] = A[j];
+        j = j + 1;
+      }
+    }
+  }
+
+  return sortDesc(result);
+
+  /*
   return result.sort((a, b) => {
     return b['year'] - a['year'];
   });
+  */
 }
